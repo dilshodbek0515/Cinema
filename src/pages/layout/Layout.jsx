986ea511e9ebaf7af8a08ccc0fae1c4ac8e605c0
dilshod <dilshodx516@gmail.com>
2024-11-29 22:32:38 +0500

    cinema

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
-const Layout = ({ sidebar, setSidebar }) => {
+const Layout = ({ sidebar, setSidebar, DarcMode, setDarcMode }) => {
   return (
     <div>
-      <Header sidebar={sidebar} setSidebar={setSidebar} />
+      <Header
+        sidebar={sidebar}
+        setSidebar={setSidebar}
+        DarcMode={DarcMode}
+        setDarcMode={setDarcMode}
+      />
       <main>
         <Outlet />
       </main>
