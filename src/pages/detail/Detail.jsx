dc1a24d75f897e86917511e1a49513bfb5f92c15
dilshod <dilshodx516@gmail.com>
2024-12-07 16:55:11 +0500

    cinema

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -15,27 +15,12 @@ const Detail = () => {
   }, [])
   return (
     <div className='bg-primary dark:bg-slate-200 w-full'>
-      <div className='w-full min-h-screen relative'>
+      <div className='px-20 h-[700px] rounded-xl max-xl:px-5 max-lg:h-96 max-[400px]:h-52'>
         <img
-          className='w-full h-full object-cover bg-no-repeat absolute top-0 left-0'
+          className='container h-full bg-cover top-0 left-0 rounded-xl'
           src={import.meta.env.VITE_IMAGE_URL + data?.backdrop_path}
           alt='img'
         />
-        <div className='relative container py-10 px-20 flex gap-20 max-lg:px-10'>
-          <img
-            className='w-[500px] h-[600px] rounded-xl bg-cover sticky top-4 max-lg:hidden'
-            src={import.meta.env.VITE_IMAGE_URL + data?.poster_path}
-            alt='img'
-          />
-          <div className='flex flex-col gap-10'>
-            <h2 className='text-7xl text-white max-2xl:text-2xl max-lg:text-5xl max-sm:text-2xl'>
-              {data?.title}
-            </h2>
-            <p className='text-white text-xl w-[600px] max-2xl:text-lg max-2xl:w-96 max-lg:w-full max-sm:text-sm'>
-              {data?.overview}
-            </p>
-          </div>
-        </div>
       </div>
       <div className='w-full flex items-center justify-center bg-primary dark:bg-slate-200 pt-20'>
         <div className='w-96 h-auto flex justify-center items-center gap-2 rounded-xl max-sm:flex-col max-sm:gap-10'>
